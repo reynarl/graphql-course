a453be68a8c5533847b946d1da3dc7b9f9d013a8
Reeyna <reeynarl@gmail.com>
2023-01-10 16:53:34 -0600

    Add schema and types

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,33 @@
 const graphql = require('graphql')
+
+const {GraphQLObjectType, GraphQLString} = graphql
+
+const CourseType = new GraphQLObjectType({
+  name: 'Course', //nombre del tipo
+  fields: () => ({
+    //definiendo tipos
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    language: {type: GraphQLString},
+    date: {type: GraphQLString}
+  })
+})
+
+const RootQuery = new GraphQLObjectType ({
+  name: 'RootQueryType',
+  fields: {
+    course: {
+      type: CourseType,
+      // el argumento que por el cual se buscará
+      args: {
+        id: { type: GraphQLString }
+      },
+
+      //codigo que accederá a la BD
+      resolve(parent, args) {
+        //nos devolverá un curso coincidente con la id que estará en la BD
+        return 
+      }
+    }
+  }
+})
