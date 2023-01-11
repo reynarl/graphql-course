1193c18eba6977955e8bf8a3378597051b192a84
Reeyna <reeynarl@gmail.com>
2023-01-10 18:22:05 -0600

    Add schema for professor

--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,59 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString} = graphql
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLSchema} = graphql
+
+var courses = [
+  {
+    id: '1',
+    name: 'Curso JS',
+    language: 'Javascript',
+    date: '2022'
+  },
+  {
+    id: '2',
+    name: 'Curso Python',
+    language: 'Python',
+    date: '2022'
+  },
+  {
+    id: '3',
+    name: 'Curso POO',
+    language: 'Java',
+    date: '2022'
+  },
+  {
+    id: '4',
+    name: 'Curso BD',
+    language: 'SQL',
+    date: '2022'
+  }
+]
+
+let professor = [
+  {id: '1', name: 'Fernando', age: 24, active: true},
+  {id: '2', name: 'Jesús', age: 25, active: true},
+  {id: '3', name: 'Guadalupe', age: 30, active: true},
+  {id: '4', name: 'Cristian', age: 28, active: true}
+]
 
 const CourseType = new GraphQLObjectType({
   name: 'Course', //nombre del tipo
   fields: () => ({
     //definiendo tipos
-    id: {type: GraphQLString},
+    id: {type: GraphQLID},
     name: {type: GraphQLString},
     language: {type: GraphQLString},
     date: {type: GraphQLString}
+  })
+})
+
+const ProfessorType = new GraphQLObjectType({
+  name: 'Professor',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    active: { type: GraphQLBoolean }
   })
 })
 
@@ -20,14 +64,28 @@ const RootQuery = new GraphQLObjectType ({
       type: CourseType,
       // el argumento que por el cual se buscará
       args: {
-        id: { type: GraphQLString }
+        id: { type: GraphQLID }
       },
 
-      //codigo que accederá a la BD
+      //resolve() se encargará de accederá a la BD
       resolve(parent, args) {
         //nos devolverá un curso coincidente con la id que estará en la BD
-        return 
+        return courses.find(curso => curso.id === args.id)
+      }
+    },
+    professor: {
+      type: ProfessorType,
+      args: {
+        name: {type: GraphQLString}
+      },
+      resolve(parent, args){
+        return professor.find(profesor => profesor.name === args.name)
       }
     }
   }
+})
+
+//exportamos schema
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
